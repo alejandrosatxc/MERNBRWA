338ea3b51e74f2c257952e39359a58d52cad789b
Alejandro Zapien <alejandrozapien@Alejandros-MacBook-Air.local>
2021-04-13 21:02:32 -0500

    Created navbar, footer, and main components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Navbar from "./components/navbar.component";
+import './css/custom.css';
+//import './css/theme.css';
+
+import BRNavbar from "./components/navbar.component";
+import Footer from "./components/footer.component";
+import Main from "./components/main.component";
 
 function App() {
   return (
     <Router>
-      
-      <div className="container">
-        <a id="start"></a>
-          <Navbar />
-      </div>
+          <BRNavbar />
+          <Main />
+          <Footer />
     </Router>
   );
 }
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,79 +1,36 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from "../img/brlogo_sm.png";
-import '../css/custom.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../css/theme.css';
+import { Col, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import logo from '../img/brlogo_sm.png';
 
+import '../css/stack-interface.css';
 
+export default class BRNavbar extends Component {
+    render () {
+        return(
+            <Navbar id="menu1" className="lawnavbg" fixed="top" bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                    <img className="logo" alt="logo" src={logo} />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Item><Nav.Link href="http://www.bellripper.com/">Home</Nav.Link></Nav.Item>
+                        <Nav.Link href="http://www.bellripper.com/aboutus.html">About Us</Nav.Link>
+                        <NavDropdown title="Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="http://www.bellripper.com/lifeandestateplanning.html">Life and Estate Planning</NavDropdown.Item>
+                            <NavDropdown.Item href="http://www.bellripper.com/mediation.html">Mediation</NavDropdown.Item>
+                            <NavDropdown.Item href="http://www.bellripper.com/familylaw.html">Family Law</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="http://www.bellripper.com/new-clients.html">New Clients</Nav.Link>
+                        <Nav.Link href="http://www.bellripper.com/resources.html">Resources</Nav.Link>
+                        <Nav.Link href="http://www.bellripper.com/contact-us.html">Contact Us</Nav.Link>
+        
+                        {/* TODO Create Signout/user portal button */}
 
-
-export default class Navbar extends Component {
-    render() {
-        return (
-        <div className="nav-container hidden-print">
-			<div className="bar bar--xs visible-xs">
-				<div className="container">
-					<div className="row">
-						<div className="col-xs-3 col-sm-2">
-							<a href="index.php">
-								<img class="logo" alt="logo" src={logo} />
-							</a>
-						</div>
-					    <div class="col-xs-9 col-sm-10 text-right">
-						    <a href="#" class="hamburger-toggle" data-toggle-class="#menu1;hidden-xs">
-							    <i class="icon icon--sm stack-interface stack-menu"></i>
-						    </a>
-					    </div>
-				    </div>
-			    </div>
-		    </div>
-            <nav id="menu1" className="lawnavbg bar bar--xs bar-1 hidden-xs bar--absolute pos-fixed">
-			    <div className="container">
-				    <div className="row">
-					    <div className="col-md-1 col-sm-2 hidden-xs">
-						    <div className="bar__module">
-							    <img className="logo" alt="logo" src={logo} />
-						    </div>
-					    </div>
-					    <div className="col-md-11 col-sm-12 text-right text-left-xs text-left-sm">
-						    <div className="bar__module">
-							    <ul className="menu-horizontal text-left">
-								    <li><a href="http://www.bellripper.com/">Home</a></li>
-								    <li><a href="http://www.bellripper.com/aboutus.html">About Us</a></li>
-                                    <li className="dropdown">
-									    <span className="dropdown__trigger">Services</span>
-									    <div className="dropdown__container">
-										    <div className="container">
-											    <div className="row">
-												    <div className="dropdown__content col-md-3 col-sm-6">
-													    <ul className="menu-vertical">
-														    <li><a href="http://www.bellripper.com/lifeandestateplanning.html">Life and Estate Planning</a></li>
-														    <li><a href="http://www.bellripper.com/mediation.html">Mediation</a></li>
-														    <li><a href="http://www.bellripper.com/familylaw.html">Family Law</a></li>
-													    </ul>
-												    </div>
-											    </div>
-										    </div>
-									    </div>
-								    </li>
-								    <li><a href="http://www.bellripper.com/new-clients.html">New Clients</a></li>
-								    <li><a href="http://www.bellripper.com/resources.html">Resources</a></li>
-								    <li><a href="http://www.bellripper.com/contact-us.html">Contact Us</a></li>
-									<li>
-										<a className="btn btn--sm btn--primary type--uppercase" href="signin.php">
-											<span class="btn__text">
-												User Portal
-											</span>
-										</a>
-									</li>
-							    </ul>
-						    </div>
-					    </div>
-				    </div>
-			    </div>
-		    </nav>
-        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
