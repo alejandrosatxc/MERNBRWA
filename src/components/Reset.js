77062783c727ec2a1b552631b4d4396cb81e0b7e
Alejandro <Alejandro@satxconsultants.com>
2021-04-17 13:03:04 -0500

    Created Reset component template

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
+import { Formik, Form } from 'formik'
+import TextInputLiveFeedback from './TextInputLiveFeedback'
 
 const Reset = () => {
     return (
-        <div>
-            
-        </div>
+        <Card>
+            <h2>Activate Account</h2>
+            <p>
+                To have an activation link sent to you, enter the email address
+                you registered with below.
+            </p>
+            <Formik>
+                <Form>
+                    <TextInputLiveFeedback
+                      label="Email"
+                      placeholder="Email"
+                      name="email"
+                      id="email"
+                      type="email"
+                    />
+                    <button type="submit">Send Activation Email</button>
+                </Form>
+            </Formik>
+            <span class="type--fine-print block">Don't have an account yet?
+			    <a href="signup.php">Create an account</a>
+            </span>
+            <span class="type--fine-print block">Forgot your username or password?
+                <a href="reset.php">Reset your password</a>
+            </span>
+        </Card>
     )
 }
 
