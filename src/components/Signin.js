e06c2928e78a6936b4e8e1b3adc98fd1466ab0cd
Alejandro <alejadro@satxconsultants.com>
2021-04-22 01:17:42 -0500

    Converted Signin from class to arrow function

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Card, Button } from 'react-bootstrap';
+import { Form, Card, Button } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
-export default class Signin extends Component {
-    render() {
-        return(
-            <Card id="Signin">
-                <h3>Already have an account?</h3>
-                <p>
-                    Sign in to fill out forms and manage your account.
-                </p>
-                <Form>
-                    <Form.Group controlId="signin.email">
-                        <Form.Label srOnly="True">Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="Email Address"></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="signin.password">
-                        <Form.Label srOnly="true">Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password"></Form.Control>
-                    </Form.Group>
-                    <Link to="/dashboard" >
-                        <Button>Sign in</Button>       
-                    </Link>
-                    <p>Forgot your password? Click <Link to="/reset">here to reset</Link>  your password</p>                     
-                </Form>
-            </Card>
-        );
-    }
+const Signin = ({setToken}) => {
+    
+    return(
+        <Card id="Signin">
+            <h3>Already have an account?</h3>
+            <p>
+                Sign in to fill out forms and manage your account.
+            </p>
+            <Form>
+                <Form.Group controlId="signin.email">
+                    <Form.Label srOnly="True">Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="Email Address"></Form.Control>
+                </Form.Group>
+                <Form.Group controlId="signin.password">
+                    <Form.Label srOnly="true">Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"></Form.Control>
+                </Form.Group>
+                <Link to="/dashboard" >
+                    <Button>Sign in</Button>       
+                </Link>
+                <p>Forgot your password? Click <Link to="/reset">here to reset</Link>  your password</p>                     
+            </Form>
+        </Card>
+    );
+
 }
