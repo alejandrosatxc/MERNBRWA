e2dd0ddf87d9ee7c6181e08d013fab4d1db8c035
Alejandro <alejadro@satxconsultants.com>
2021-04-22 13:08:13 -0500

    Combined Login and greeting

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,11 +26,25 @@ const Login = ({setToken}) => {
             email,
             password
         });
+        console.log(email, password, token )
         setToken(token);
     }
 
     return(
         <Card id="Login">
+            <h3> User Portal</h3>
+            <p>
+                The User Portal allows you to fill out forms so that your documents may be generated
+                for review by an attorney. The forms will be available to fill out at your leisure and will
+                provide you a preview of your documents as you fill out the forms.
+                The final documents, that were reviewed by an attorney, will then be uploaded to the
+                User Portal for your review.
+
+                Sign In or Create an account to begin.
+            </p>
+            <Link to="/signup">
+                <Button>Create an Account</Button>
+            </Link>
             <h3>Already have an account?</h3>
             <p>
                 Sign in to fill out forms and manage your account.
