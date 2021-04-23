83d808548865e347bd18fa0173b15aa574e9d804
Alejandro <alejadro@satxconsultants.com>
2021-04-23 03:28:45 -0500

    imports for redux implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import { Provider } from 'react-redux'
+import store from './store'
+import { loadUser } from './actions/authActions'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useToken from './hooks/useToken'
 //import './css/custom.css';
 //import './css/theme.css';
@@ -11,23 +16,30 @@ import BRNavbar from "./components/BRNavbar";
 import Footer from "./components/Footer";
 import Dashboard from "./components/Dashboard";
 import Login from './components/Login'
+import { USER_LOADING } from './actions/types';
 
 function App() {
 
   const {token, setToken} = useToken();
 
+  useEffect(() => {
+    store.dispatch(loadUser());
+  });
+
   return (
-    <Router>
-      <BRNavbar/>
-      <Container className="main-container">
-        {/* token defines loggedin or logged out, show portal if loggedout, show dash if logged in */}
-        {token
-          ? <Route path="/" component={Dashboard} />
-          : <Login setToken={setToken} /> 
-        }
-      </Container>       
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <BRNavbar/>
+        <Container className="main-container">
+          {/* token defines loggedin or logged out, show portal if loggedout, show dash if logged in */}
+          {token
+            ? <Route path="/" component={Dashboard} />
+            : <Login setToken={setToken} /> 
+          }
+        </Container>       
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 
