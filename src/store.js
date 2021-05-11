436fabeba14ba0c4cee584a38985dbe4fc8a0a95
Alejandro <Alejandro@satxconsultants.com>
2021-05-10 19:46:15 -0500

    Rewrote code to allow Redux-dev-tools trace()

--- src/store.js
+++ src/store.js
@@ -2,13 +2,24 @@ import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+import { composeWithDevTools } from "redux-devtools-extension";
+
 const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = composeWithDevTools({ 
+    trace: true, 
+    traceLimit: 25 
+}); 
+const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(...middleware) 
+));
+
+/*
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
-
+*/
 export default store
