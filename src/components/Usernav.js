4f5b3d49d3187fd196e1e5281e66630025e49c03
Alejandro <Alejandro@satxconsultants.com>
2021-05-10 19:47:31 -0500

    Changed onClick() to call internal function

--- src/components/Usernav.js
+++ src/components/Usernav.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import { Card, Nav } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+import { Card, Nav, NavLink } from 'react-bootstrap'
+
 import { Link } from 'react-router-dom'
+import { logout } from '../actions/authActions'
 
 const Usernav = () => {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
     return (
         <Card>
             <Nav className="flex-column">
@@ -10,7 +18,9 @@ const Usernav = () => {
                 <Nav.Item><Link to="/dashboard">Home</Link></Nav.Item>
                 <Nav.Item><Link to="/dashboard/survey">Intake</Link></Nav.Item>
                 <Nav.Item><Link to="/dashboard/account">My Account</Link></Nav.Item>
-                <Nav.Item><Link to="/">Sign Out</Link></Nav.Item>
+                <Nav.Item>
+                    <NavLink onClick={handleLogout} href='#'>Logout</NavLink>
+                </Nav.Item>
             </Nav>
         </Card>
     )
