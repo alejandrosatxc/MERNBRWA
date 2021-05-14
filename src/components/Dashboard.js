6f588b2b03e7e1b83c5af44a95e7fbf79d6818ad
Alejandro <Alejandro@satxconsultants.com>
2021-05-14 14:05:33 -0500

    Only survey component rendered for now

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import Usernav from './Usernav'
 import Useraccount from './Useraccount'
 import Survey from './Survey'
 
-const Dashboard = ({
-  isAuthenticated,
-  error,
-  login,
-  clearErrors,
-}) => {
+const Dashboard = () => {
 
 
-        return(
-          <Router>  
-            <Row>
-                <Col>
-                    <Usernav/>
-                </Col>
-                <Col>
-                  <Switch>  
-                    <Route path="/dashboard/survey" component={Survey} />
-                    <Route path="/dashboard/account" component={Useraccount} />
-                  </Switch>
-                </Col>
-            </Row>
-          </Router>
-        );
+  return(
+    <Survey/>
+  );
 }
 
 export default Dashboard
