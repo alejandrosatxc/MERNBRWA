67fa27df1da8e25fb377f3cfe02b0d08fb5ea8f5
Alejandro <Alejandro@satxconsultants.com>
2021-05-14 14:04:25 -0500

    Quick schema for survey submission

--- backend/models/usersurveys.model.js
+++ backend/models/usersurveys.model.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSurveysSchema = new Schema({
-    usurveyid: { type: String, required: true, default: null },
-    userid: { type: String, required: true, default: null },
-    surveyid: { type: String, requied: true, defualt: null},
-    paid: { type: String, requied: true, defualt: null},
-    status: { type: String, requied: true, defualt: null},
-    startdate: { type: Date, requied: true, defualt: null},
-    finaldate: { type: Date, requied: true, defualt: null},
-    data: { type: String, requied: true, defualt: null}, //The type might have to be JSON? 
-    active: { type: String, requied: true, defualt: null},
+const userSurveySchema = new Schema({
+    usurveyid: { type: String, default: null },
+    //userid: { type: String, required: true, default: null },
+    surveyid: { type: String, required: true, default: null},
+    //paid: { type: String, required: true, default: null},
+    //status: { type: String, required: true, default: null},
+    //startdate: { type: Date, required: true, default: null},
+    //finaldate: { type: Date, required: true, default: null},
+    data: { type: Object, required: true, default: null}, //The type might have to be JSON? 
+    active: { type: String, required: true, default: null},
 
 });
 
