61bf4e651516061bffc58e7b8175e7d36c23adb7
Alejandro <alejadro@satxconsultants.com>
2021-05-17 05:39:44 -0500

    changed code order

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) =>  {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
 app.use(cors());
 app.use(express.json());
 
@@ -36,7 +26,17 @@ const authRouter = require('./routes/api/auth');
 app.use('/api/users', usersRouter);
 app.use('/api/surveys', surveysRouter);
 app.use('/api/auth', authRouter);
-//app.use('/login', loginRouter);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>  {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
