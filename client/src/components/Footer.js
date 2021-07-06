9b7a6d852c4b6285ddd7fb9f459f113c492d8433
alejandrosatxc <alejandro@satxconsultants.com>
2021-07-06 15:29:09 -0500

    Added email to the footer for contact and bug info

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,8 @@ export default class Footer extends Component {
         return (
         <Container>    
             <p>
+                <a href="mailto:alejandro@satxconsultants.com?subject=Bug/App Feedback">Send Feedback/report bugs</a>
+                <br></br><br></br>
                 Disclaimers:
                 <br></br>
                 Bell Ripper PLLC is responsible for the content of this website.  The principal place of business for Bell Ripper PLLC is 1002 N. Flores St., San Antonio, Texas 78212.  Unless otherwise indicated, attorneys are not Board Certified.
