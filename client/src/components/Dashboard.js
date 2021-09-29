74fb304676ca727d0889b72237a7127faa4c161a
alejandrosatxc <alejandro@satxconsultants.com>
2021-09-29 13:31:07 -0500

    Check if user has complete the intakeform at login

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Survey from './Survey'
+import Usernav from './Usernav'
 
 const Dashboard = () => {
 
-
+  //const userIntakeComplete = useSelector(state => state.auth.user.intake_complete)
   return(
     <Survey/>
   );
