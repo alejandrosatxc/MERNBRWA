4054a56c0f012b3372289c5ee94f49c11577ed18
alejandrosatxc <alejandro@satxconsultants.com>
2021-10-01 13:32:19 -0500

    TODO comments

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,14 +5,15 @@ import Useraccount from './Useraccount'
 import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
-
+  //TODO if intakes are complete, give user the option to review the last intake form they have submitted
   const userIntakeComplete = useSelector(state => state.auth.user.intake_complete)
   return(
     <>
+      <Usernav/>
       {userIntakeComplete
         ? <Useraccount/>
-        : <Survey/>
-      }
+        : <Survey/>}
+
     </>
   );
 }
