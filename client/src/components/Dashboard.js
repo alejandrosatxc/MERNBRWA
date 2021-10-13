1314e846cc6f2c34a869cc50f48eb90f5a30284d
AlejandroZapien <alejandrozapien09@gmail.com>
2021-10-13 14:49:09 -0500

    Dashboard Experiment

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Survey from './Survey'
 import Usernav from './Usernav'
 import Useraccount from './Useraccount'
 import { useSelector } from 'react-redux';
 
+import { Tabs, Tab, Card} from 'react-bootstrap'
+import UserForms from './UserForms'
+
 const Dashboard = () => {
   //TODO if intakes are complete, give user the option to review the last intake form they have submitted
   const userIntakeComplete = useSelector(state => state.auth.user.intake_complete)
+  const surveyid = 1 //intake form
+  const surveyMode = 'edit'
+
+  
+
+  //TODO check User.actve_forms for a surveyid of 1 where form_status = Complete
   return(
     <>
+      {userIntakeComplete ?
       <Usernav/>
-      {userIntakeComplete
-        ? <Useraccount/>
-        : <Survey/>}
-
+      : <Survey surveyid={surveyid} surveyMode={surveyMode}/>}
     </>
   );
 }
