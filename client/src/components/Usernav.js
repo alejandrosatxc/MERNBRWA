30f251a37b1fff28b74884006ccac2b398c8345c
AlejandroZapien <alejandrozapien09@gmail.com>
2021-10-13 14:48:42 -0500

    Usernav is tabs now

--- client/src/components/Usernav.js
+++ client/src/components/Usernav.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Card, Nav, NavLink } from 'react-bootstrap'
+import { Card, Tab, Tabs } from 'react-bootstrap'
 
 import { Link } from 'react-router-dom'
 import { logout } from '../actions/authActions'
-
+import UserForms from './UserForms'
+import Useraccount from './Useraccount'
 const Usernav = () => {
     const dispatch = useDispatch();
+    const [activeComponent, setActiveComponet] = useState();
     const username = useSelector(state => state.auth.user.firstName + " " + state.auth.user.lastName)
     const handleLogout = () => {
         dispatch(logout())
     }
     return (
-        <Card>
-            <Nav className="flex-column">
-                <h2>{username}</h2>
-                <Nav.Item><Link to="/dashboard">Home</Link></Nav.Item>
-                <Nav.Item><Link to="/dashboard/survey">Intake</Link></Nav.Item>
-                <Nav.Item><Link to="/dashboard/account">My Account</Link></Nav.Item>
-                <Nav.Item>
-                    <NavLink onClick={handleLogout} href='#'>Logout</NavLink>
-                </Nav.Item>
-            </Nav>
-        </Card>
+        <>
+            <Tabs variant="pills" defaultActiveKey="Userforms" id="userNav">
+                <Tab eventKey="Userforms" title="My Forms">
+                    <UserForms/>
+                </Tab>
+                <Tab eventKey="Userdocuments" title="MyDocuments">
+                    <h1>User documents will render here</h1>
+                </Tab>
+                <Tab eventKey="Useraccount" title="Account">
+                    <Useraccount/>
+                </Tab>
+                <Tab eventKey="Store" title="Store">
+                    <h1>Store stuffs go here</h1>
+                </Tab>
+            </Tabs>
+        </>
     )
 }
 
