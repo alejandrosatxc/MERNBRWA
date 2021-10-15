c1fea57065a2fc2237aa8c31c0a3eafb1cd6fc6b
alejandrosatxc <alejandro@satxconsultants.com>
2021-10-15 00:58:22 -0500

    automating intake and attorney form assignment

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 
 //User Model
 const User = require('../../models/users.model');
+const userSubmissions = require('../../models/usersubmissions.model')
 const bcrypt = require('bcryptjs');
 const { config } = require('dotenv');
+const userSubmission = require('../../models/usersubmissions.model');
 
 // @route   POST  api/users
 // @desc    Register new user
@@ -23,14 +25,23 @@ router.post('/', (req, res) => {
     //Check for existing user/email
     User.findOne({ email }) //This can also be written as User.findOne({ email })
       .then(user => {
-          if(user) return res.status(400).json({msg : "User already exists"});
+            if(user) return res.status(400).json({msg : "User already exists"});
 
-          const newUser = new User({
-              firstName,
-              lastName,
-              email,
-              password //Password is unhashed
-          });
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                password //Password is unhashed
+            });
+
+            //Create an intake form
+            const intakeForm = new userSubmission({
+                usurveyid : newUser._id,
+                surveyid : 1,
+                active : 1,
+                form_status : "new",
+                userResponses : null
+            })
 
           // Create salt & hash
           bcrypt.genSalt(10, (err, salt) => {
@@ -38,8 +49,19 @@ router.post('/', (req, res) => {
                   if(err) throw err;
                   newUser.password = hash;
                   newUser.intake_complete = 0;
+                  newUser.active_forms.push({
+                      form_id: intakeForm._id,
+                      surveyid: 1,
+                      form_status: "new"
+                  })
                   newUser.save() //this returns a promise
                     .then(user => {
+                        //Save the new intake form in db
+                        intakeForm.save()
+                            .then(() => {
+                                console.log("Saved: " + intakeForm)
+                            })
+                            .catch(err => res.status(400).json('intakeForm not saved to db'))
 
                         //Create JWT
                         jwt.sign(
@@ -48,7 +70,7 @@ router.post('/', (req, res) => {
                             { expiresIn : 3600 },
                             (err, token) => {
                                 if(err) throw err;
-                                res.json({
+                                res.json({ //Respond with a token so user can be authenticated an logged in
                                     token,
                                     user: {
                                         _id: user._id,
