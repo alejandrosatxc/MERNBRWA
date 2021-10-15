439b75f64ae3abab44b0059266446850a2d02672
alejandrosatxc <alejandro@satxconsultants.com>
2021-10-15 00:58:39 -0500

    Added routes for ui control and navigation

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect }from 'react';
+import { Route, withRouter, useLocation } from 'react-router-dom'
 import Survey from './Survey'
-import Usernav from './Usernav'
-import Useraccount from './Useraccount'
+import UserNav from './UserNav'
+import UserAccount from './UserAccount'
+import UserForms from './UserForms';
+import UserDocuments from './UserDocuments'
+import ManageUsers from './ManageUsers'
 import { useSelector } from 'react-redux';
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   //TODO if intakes are complete, give user the option to review the last intake form they have submitted
   const userIntakeComplete = useSelector(state => state.auth.user.intake_complete)
+  const auth = useSelector(state => state.auth)
+  const location = useLocation();
+
+  //The dynamic form route will be set in the UserForms component via a click by the user
+
+  useEffect(() => {
+    if(!userIntakeComplete && auth.user.role === "Client" ) {
+      props.history.push(`/form/1/edit/${auth.user.active_forms[0].form_id}`) //Force an intake survey
+    }
+  }, [auth.user, location.pathname]) //Only run when user logs/loads in or user tries to navigate away
+
   return(
     <>
-      <Usernav/>
-      {userIntakeComplete
-        ? <Useraccount/>
-        : <Survey/>}
-
+      {(!userIntakeComplete && auth.user.role === "Client") ? null : <UserNav/>}
+      <Route path="/myforms" component={UserForms} />
+      <Route path="/mydocuments" component={UserDocuments} />
+      <Route path="/account" component={UserAccount} />
+      <Route path="/form/:surveyid/:surveyMode/:userSubmission_id" component={Survey} />
+      {
+        (auth.user.role === 'Admin')
+        ? <Route path="/manageusers" component={ManageUsers} />
+        : null
+      }
     </>
   );
 }
 
-export default Dashboard
+export default withRouter(Dashboard)
