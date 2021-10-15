c9a8d066c09a1cb805fd63df9f6cdfc60ab87114
alejandrosatxc <alejandro@satxconsultants.com>
2021-10-15 00:55:47 -0500

    Renders list of active forms for the user

--- client/src/components/UserForms.js
+++ client/src/components/UserForms.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, Container, Row, Col, Button } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 //This component will show a list of all form a user has completed
 //and allow them to review thier submission
@@ -9,17 +10,46 @@ const UserForms = () => {
     const user = useSelector(state => state.auth.user)
 
     const formList = user.active_forms.map((form, index) => {
+
+        //TODO this naming switchcase should be handled differently in a different location
+        var name = ''
+        var surveyMode = ''
+        switch(form.surveyid) {
+            case 1:
+                name = "Intake"
+                surveyMode = "display"
+                break;
+            case 2:
+                name = "Attorney Form"
+                surveyMode = "edit"
+                break;
+            default:
+                name = "Undefined"
+        }
+
         return (
             <Card>
                 <Card.Img variant="top"/>
                 <Card.Body>
-                    <Card.Title>Surveyid: {form.surveyid}</Card.Title>
-                    <Card.Text>Form status: {form.form_status}</Card.Text>
-                    <Button variant="primary">View</Button>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <p>Form status: {form.form_status}</p>
+                        {user.role === "Admin"
+                            ? <p>For: {form.user_name}</p>
+                            : null
+                        }
+                    </Card.Text>
+
+                    <Link to={`/form/${form.surveyid}/${surveyMode}/${form.form_id}`}>{(surveyMode === 'display') ? "View" : "Edit"}</Link>
+                    <br/>
+                    {
+                    //<Link to={`/form/${form.surveyid}/edit`} >Edit</Link>
+                    }
                 </Card.Body>
             </Card>
         )
     })
+
     return(
         <Card>
            <Container>
