968c578c4480900eb6717900c581b983dbab1b30
alejandrosatxc <alejandro@satxconsultants.com>
2021-10-15 00:57:52 -0500

    chnaged some configs to make life easier

--- models/usersubmissions.model.js
+++ models/usersubmissions.model.js
@@ -10,9 +10,9 @@ const userSubmissionSchema = new Schema({
     //status: { type: String, required: true, default: null},
     //startdate: { type: Date, required: true, default: null},
     //finaldate: { type: Date, required: true, default: null},
-    userResponses: { type: Object, required: true, default: null}, //The type might have to be JSON? 
+    userResponses: { type: Object, required: false, default: null}, //The type might have to be JSON? 
     active: { type: String, required: true, default: null},
-    form_status: {type: String, required: true, default: null}
+    form_status: {type: String, required: true, default: "Incomplete"}
     }, {
         timestamps: true
     });
