0ec80bc45cadca17fd77ecf66836942f1bc6f84e
alejandrosatxc <69367719+alejandrosatxc@users.noreply.github.com>
2021-10-15 01:29:54 -0500

    Update UserNav.js

--- client/src/components/UserNav.js
+++ client/src/components/UserNav.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Card, Tab, Tabs } from 'react-bootstrap'
+import { Card, Tab, Tabs, Nav } from 'react-bootstrap'
 
 import { Link } from 'react-router-dom'
 import { logout } from '../actions/authActions'
